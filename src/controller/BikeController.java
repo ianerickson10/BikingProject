69387f30792f9a2553698d5dc91846bbcb513fe5
Ian Erickson <19ierickson@gmail.com>
2018-09-20 13:00:10 -0600

    tried adding questions...

--- src/controller/BikeController.java
+++ src/controller/BikeController.java
@@ -6,6 +6,10 @@ import javax.swing.JOptionPane;
 
 public class BikeController
 {
+	//Declarations
+	private Bike bikeDefault;
+	private Bike bikeUser;
+	
 	
 	/**
 	 * Builds controller
@@ -16,10 +20,14 @@ public class BikeController
 	}
 	
 	/**
-	 * Where the program calls methods to run here
+	 * Where the program calls methods to run
 	 */
 	public void start()
 	{
+		String userInput = JOptionPane.showInputDialog("Good job, you just went on a ride. What would you like to title it?");
+		
+		String userInput = JOptionPane.showInputDialog("How many miles did you ride?");
+		
 		
 	}
 }
