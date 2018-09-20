bba1bcc551b0a2b30d7f7cd827218686f712dce3
Ian Erickson <19ierickson@gmail.com>
2018-09-20 12:11:37 -0600

    fixed constructors

---
+++ src/model/Bike.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Bike
+{
+	//Declaration Section
+	private String rideLocation;
+	private double distance;
+	private int mins;
+	private int elevationGain;
+	private double avgSpeed;
+	
+	
+	public Bike()
+	{
+		
+	}
+}
