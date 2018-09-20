9adb52eae4107f80a97b35313b37dc8b781804e8
Ian Erickson <19ierickson@gmail.com>
2018-09-20 11:53:00 -0600

    Created project and start mechanism

---
+++ src/controller/BikeController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class BikeController
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/controller/BikeRunner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class BikeRunner
+{
+	public static void main (String [] args)
+	{
+		BikeController app = new BikeController();
+		app.start();
+	}
+}
