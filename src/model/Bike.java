1777729dbc890570fa2f177c1c2a3c09f813e1d9
Ian Erickson <19ierickson@gmail.com>
2018-09-20 12:21:03 -0600

    set default constructors

--- src/model/Bike.java
+++ src/model/Bike.java
@@ -3,15 +3,23 @@ package model;
 public class Bike
 {
 	//Declaration Section
-	private String rideLocation;
+	private String rideName;
 	private double distance;
 	private int mins;
 	private int elevationGain;
 	private double avgSpeed;
 	
 	
+	/**
+	 * Default constructors
+	 * sets them to -1 so I know there is no info
+	 */
 	public Bike()
 	{
-		
+		this.rideName = "unnamed ride";
+		this.distance = -1;
+		this.mins = -1;
+		this.elevationGain = -1;
+		this.avgSpeed = -1;
 	}
 }
