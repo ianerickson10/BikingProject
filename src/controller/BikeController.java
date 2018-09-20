28f8611d7966b8409a01179a735fb0fa37808daa
Ian Erickson <19ierickson@gmail.com>
2018-09-20 12:08:36 -0600

    added constructors

--- src/controller/BikeController.java
+++ src/controller/BikeController.java
@@ -2,6 +2,14 @@ package controller;
 
 public class BikeController
 {
+	//Declaration Section
+	private String rideLocation;
+	private double distance;
+	private int mins;
+	private int elevationGain;
+	private double avgSpeed;
+	
+	
 	public void start()
 	{
 		
